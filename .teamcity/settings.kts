174dd864374c2fa5e4d8f9a1b8481de07c96d3f7
brad.jones <brad.jones@TeamCity>
2021-05-20 07:47:06 +0000

    TeamCity change in 'test2' project: Change cleanup settings for project

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -31,6 +31,7 @@ project {
             all(builds = 6, days = 5)
             history(builds = 4, days = 3)
             artifacts(builds = 2, days = 1)
+            preventDependencyCleanup = true
             option("disableCleanupPolicies", false)
         }
     }
